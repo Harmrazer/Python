d4c26b94a3ee2b2e3996740c38334fd8e0f15b53
Harmrazer <harmrazer@gmail.com>
2022-10-18 18:24:00 +0300

    s1

---
+++ S1.py
@@ -0,0 +1,4 @@
+number = int (input('Input number: \n '))
+print('Quadro numb {1} = {0}'.format(number, number**2), end='///')
+print(f'Quadro numb {number} = {number**2}')
+print('Quadro numb', number, '=', number**2)
