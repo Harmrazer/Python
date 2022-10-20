32f367a68831e3598b0d4d9a24eab6aa699a0dc9
Harmrazer <harmrazer@gmail.com>
2022-10-20 16:24:43 +0300

    tt

--- Test.py
+++ Test.py
@@ -1,1 +1,6 @@
-dsfgfsdgdfsgdf
+print('Задача 24')
+print('')
+
+lst = list(map(float, input("Введите числа через пробел:\n").split()))
+new_lst = [round(i%1,2) for i in lst if i%1 != 0]
+print(max(new_lst) - min(new_lst))
